4e3a65d1fb731cc667142ca99e3a2ae3339a9251
Nitesh Nagar <159615163+mrzitesh@users.noreply.github.com>
2025-01-21 02:13:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+let data = {};
+
+// Load JSON data
+fetch('doctors_medicine.json')
+    .then(response => response.json())
+    .then(json => {
+        data = json;
+        loadAreas();
+    });
+
+// Populate Area Dropdown
+function loadAreas() {
+    let areaSelect = document.getElementById("areaSelect");
+    Object.keys(data).forEach(area => {
+        let option = document.createElement("option");
+        option.value = area;
+        option.textContent = area;
+        areaSelect.appendChild(option);
+    });
+}
+
+// Populate Doctors Based on Selected Area
+function loadDoctors() {
+    let area = document.getElementById("areaSelect").value;
+    let doctorSelect = document.getElementById("doctorSelect");
+    doctorSelect.innerHTML = "<option value=''>--Select Doctor--</option>";
+
+    if (area && data[area]) {
+        Object.keys(data[area]).forEach(doctor => {
+            let option = document.createElement("option");
+            option.value = doctor;
+            option.textContent = doctor;
+            doctorSelect.appendChild(option);
+        });
+    }
+}
+
+// Show Medicines & Photos Based on Selected Doctor
+function loadMedicines() {
+    let area = document.getElementById("areaSelect").value;
+    let doctor = document.getElementById("doctorSelect").value;
+    let medicineContainer = document.getElementById("medicineContainer");
+    medicineContainer.innerHTML = "";
+
+    if (area && doctor && data[area][doctor]) {
+        data[area][doctor].forEach(med => {
+            let img = document.createElement("img");
+            img.src = med.photo;
+            img.alt = med.medicine;
+            let label = document.createElement("p");
+            label.textContent = med.medicine;
+            medicineContainer.appendChild(img);
+            medicineContainer.appendChild(label);
+        });
+    }
+}
